e8fdc67c798e613a05e8860e16e5789953e34a07
Jason Sklikas <jason.sklikas@gmail.com>
2016-09-26 16:22:34 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Computes an inverse matrix but if the matrix inversion already exist
+##from a previews callculation it recals it to avoid further computations
 
-## Write a short comment describing this function
+## Creates a special Matrix that:
+##set the matrix and gets the matrix
+##set the inverse kai gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver<-NULL
+  set<- function(y) {
+    x<<- y
+    inver<-NULL
+  }
+  get<- function()x
+  set_inverse <- function(inverse) inver<<- inverse
+  get_inverse<- function() inver
+  list(set=set, get=get, set_inverse=set_inverse,
+       get_inverse=get_inverse)
 }
 
-
-## Write a short comment describing this function
+## Calculates the inverse given matrix but if it has already
+## benn calculated it retrieves the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inver<- x$get_inverse()
+  if (!is.null(inver)){
+      message("Getting cache data")
+    return(inver)
+  }
+  data<- x$get()
+  inver<- solve(data, ...)
+  x$set_inverse
+  inver
 }
+
